0a66bbb6a302c9896fd33b0c12babd2dbfdea154
Bheemalinga <kummarabheema@gmail.com>
2023-09-09 13:01:54 +0530

    Code documentation

--- javascript.js
+++ javascript.js
@@ -19,37 +19,37 @@ dropdown.addEventListener("change", function () { // when the dropdown changes
   allowSubmit = true; // allow the form to be submitted
 });
 
-document.querySelector("form button").addEventListener("click", function(e){
-  e.preventDefault();
+document.querySelector("form button").addEventListener("click", function(e){ // when the form is submitted
+  e.preventDefault(); // prevent the default action
 
-  if(allowSubmit){
-    breed = dropdown.value;
-    displayDog(breed);
-    allowSubmit = false;
+  if(allowSubmit){ // if the form is allowed to be submitted
+    breed = dropdown.value; // set the breed to the value of the dropdown
+    displayDog(breed); // displaying the dog
+    allowSubmit = false; // to prevent the form from being submitted again, changing the value to false
   }
 });
 
-document.querySelector("#next a").addEventListener("click", function(e){
-  e.preventDefault();
-  if(breed !== undefined){
-    displayDog(breed);
+document.querySelector("#next a").addEventListener("click", function(e){ // when the next button is clicked
+  e.preventDefault(); // prevent the default action
+  if(breed !== undefined){ // if the breed is defined
+    displayDog(breed); // display that breed
   }
 });
 
-function displayDog(breed){
-  let url = "https://dog.ceo/api/breed/" + breed + "/images/random";
+function displayDog(breed){ // function to display the dog
+  let url = "https://dog.ceo/api/breed/" + breed + "/images/random"; // the url for the image
 
-  while(breedImage.firstChild){
-    breedImage.removeChild(breedImage.firstChild);
+  while(breedImage.firstChild){ // while there is a first child
+    breedImage.removeChild(breedImage.firstChild); // removing that first child
   }
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      let imageUrl = data.message;
-      let img = document.createElement("img");
-      img.src = imageUrl;
-      img.alt = breed;
-      breedImage.appendChild(img);
+  fetch(url) // fetching the image
+    .then(response => response.json()) // converting the response to JSON
+    .then(data => { // using the data we got form the response
+      let imageUrl = data.message; // the image url is in data.message
+      let img = document.createElement("img"); // creating an image element to display the image
+      img.src = imageUrl; // setting the source of the image to the image url, so that the image can be displayed
+      img.alt = breed; // setting the alt text to the breed, so that the breed name is displayed if the image can't be displayed
+      breedImage.appendChild(img); // appending the image to the div to display
     });
 }
